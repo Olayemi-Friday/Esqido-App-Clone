33431959476dcc6dcd90d52cca6731d120d24c05
Olayemi-Friday <93773878+Olayemi-Friday@users.noreply.github.com>
2022-04-23 17:55:44 +0100

    Just created some file for my project

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$('#theTarget').skippr({
+    transition: 'slide',
+    speed: 1000,
+    easing: 'easeOutCirc',
+    navType: 'bubble',
+    childrenElementType: 'div',
+    arrows: true,
+    autoPlay: true,
+    autoPlayDuration: 3000,
+    keyboardOnAlways: false,
+    hidePrevious: true,
+  });
+
+  $('#theTarget2').skippr({
+    transition: 'slide',
+    speed: 1000,
+    easing: 'easeOutCirc',
+    navType: 'bubble',
+    childrenElementType: 'div',
+    arrows: true,
+    autoPlay: true,
+    autoPlayDuration: 3000,
+    keyboardOnAlways: false,
+    hidePrevious: true,
+  });
